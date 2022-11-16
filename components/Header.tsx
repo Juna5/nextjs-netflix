d08c17d0623eddce1cfd1b1f0c481603110a29cc
juna4 <muhammad.junaedi@intidata.net>
2022-11-16 20:55:50 +0700

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import {BellIcon, SearchIcon} from "@heroicons/react/outline";
+import Link from 'next/link';
+function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 0) {
+                setIsScrolled(true)
+            }else{
+                setIsScrolled(false)
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    })
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+        <div className='flex items-center space-x-2 md:space-x-10'>
+            <img 
+                width={100}
+                height={100}
+                src="https://shorturl.at/jmsN2"
+                className='cursor-pointer object-contain' 
+            />
+            <ul className='hidden space-x-4 md:flex'>
+                <li className='header-link'>Home</li>
+                <li className='header-link'>TV Shows</li>
+                <li className='header-link'>Movies</li>
+                <li className='header-link'>New & Popular</li>
+                <li className='header-link'>My List</li>
+            </ul>
+        </div>
+        <div className='flex items-center space-x-4 text-sm font-light'>
+            <SearchIcon className='hidden h-6 w-6 sm:inline'/>
+            <p className="hidden lg:inline">Kids</p>
+            <BellIcon className='h-6 w-6'/>
+            <Link href='/account'>
+            <img src="https://rb.gy/g1pwyx" alt="" />
+            </Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header
