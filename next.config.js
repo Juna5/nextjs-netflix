7a06ef00522514bc45b3ae54567885799dfc9242
juna4 <muhammad.junaedi@intidata.net>
2022-11-22 15:55:29 +0700

    fix auth

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['image.tmdb.org']
-  }
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ["image.tmdb.org", "rb.gy"],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Banner from "../components/Banner";
 import requests from "../utils/requests";
 import { Movie } from "../typings";
 import Row from "../components/Row";
+import useAuth from "../hooks/useAuth";
 
 export async function getServerSideProps() {
     const [
@@ -60,6 +61,8 @@ export default function Home({
     romanceMovies,
     documentaries,
 }: Props) {
+    const { logout, loading } = useAuth();
+    if (loading) return null;
     return (
         <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
             <Head>
